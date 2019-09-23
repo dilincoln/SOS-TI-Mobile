e9167374829ba1d3ca3859e480c71f181852fff4
Diego Lincoln <diego.lincoln@hotmail.com>
2019-09-22 22:40:58 -0300

    Ready for code!

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+})
+
+export default function App() {
+    return (
+        <>
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Seja bem vindo</Text>
+                <Text style={styles.welcome}>React Native!</Text>
+            </View>
+        </>
+    )
+}
